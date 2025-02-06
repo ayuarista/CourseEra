a8d0a6e4e6faeb589afc05aee4ef432859cb8684
ayuarista <aristaadewi8@gmail.com>
2025-02-06 11:14:25 +0800

    create course section

---
+++ src/script.js
@@ -0,0 +1,54 @@
+function course(){
+    return{
+        items:[
+            {
+                id: 1,
+                img: 'https://gradepowerlearning.com/wp-content/uploads/2018/09/how-to-use-self-study-scaled.jpeg',
+                price: 'Rp. 300.000',
+                level: 'advanced',
+                lesson: '10',
+                student: '52',
+                title: 'Learning Javascript DOM',
+                rating: '4.9',
+                weeks: '4',
+                language: 'Javascript'
+            },
+            {
+                id: 2,
+                img: 'https://media.istockphoto.com/id/1581045672/photo/education-laptop-student-computer-teenager-studying-boy-learning-homework-study-young-school.jpg?s=612x612&w=0&k=20&c=ytSpytOADNigrtZTFZkZwYmjBeEculuHc_2MLEvpKNk=',
+                price: 'Rp. 250.000',
+                level: 'beginner',
+                lesson: '8',
+                student: '45',
+                title: 'Introduction to HTML & CSS',
+                rating: '4.7',
+                weeks: '3',
+                language: 'HTML & CSS'
+            },
+            {
+                id: 3,
+                img: 'https://media.istockphoto.com/id/1214361283/photo/teenage-boy-attending-to-online-school-class.jpg?s=612x612&w=0&k=20&c=1Trs3CU6WXJDJxqWwigcqlhQ6M7motuKqxBH3kKAmLA=',
+                price: 'Rp. 400.000',
+                level: 'intermediate',
+                lesson: '12',
+                student: '60',
+                title: 'Mastering Framework React.js',
+                rating: '4.8',
+                weeks: '5',
+                language: 'Javascript'
+            },
+            {
+                id: 4,
+                img: 'https://img.freepik.com/premium-photo/group-school-children-using-laptops-class_236854-50313.jpg',
+                price: 'Rp. 350.000',
+                level: 'advanced',
+                lesson: '15',
+                student: '38',
+                title: 'Full-Stack Web Development',
+                rating: '5.0',
+                weeks: '6',
+                language: 'Javascript'
+            }
+        ]
+    }
+}
