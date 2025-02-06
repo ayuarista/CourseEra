76dc5c45932ac733f40dcf238ef6785689d9eac0
ayuarista <aristaadewi8@gmail.com>
2025-02-06 13:16:02 +0800

    create CTA

--- src/script.js
+++ src/script.js
@@ -1,54 +1,97 @@
-function course(){
-    return{
-        items:[
-            {
-                id: 1,
-                img: 'https://gradepowerlearning.com/wp-content/uploads/2018/09/how-to-use-self-study-scaled.jpeg',
-                price: 'Rp. 300.000',
-                level: 'advanced',
-                lesson: '10',
-                student: '52',
-                title: 'Learning Javascript DOM',
-                rating: '4.9',
-                weeks: '4',
-                language: 'Javascript'
-            },
-            {
-                id: 2,
-                img: 'https://media.istockphoto.com/id/1581045672/photo/education-laptop-student-computer-teenager-studying-boy-learning-homework-study-young-school.jpg?s=612x612&w=0&k=20&c=ytSpytOADNigrtZTFZkZwYmjBeEculuHc_2MLEvpKNk=',
-                price: 'Rp. 250.000',
-                level: 'beginner',
-                lesson: '8',
-                student: '45',
-                title: 'Introduction to HTML & CSS',
-                rating: '4.7',
-                weeks: '3',
-                language: 'HTML & CSS'
-            },
-            {
-                id: 3,
-                img: 'https://media.istockphoto.com/id/1214361283/photo/teenage-boy-attending-to-online-school-class.jpg?s=612x612&w=0&k=20&c=1Trs3CU6WXJDJxqWwigcqlhQ6M7motuKqxBH3kKAmLA=',
-                price: 'Rp. 400.000',
-                level: 'intermediate',
-                lesson: '12',
-                student: '60',
-                title: 'Mastering Framework React.js',
-                rating: '4.8',
-                weeks: '5',
-                language: 'Javascript'
-            },
-            {
-                id: 4,
-                img: 'https://img.freepik.com/premium-photo/group-school-children-using-laptops-class_236854-50313.jpg',
-                price: 'Rp. 350.000',
-                level: 'advanced',
-                lesson: '15',
-                student: '38',
-                title: 'Full-Stack Web Development',
-                rating: '5.0',
-                weeks: '6',
-                language: 'Javascript'
-            }
-        ]
-    }
+function course() {
+  return {
+    items: [
+      {
+        id: 1,
+        img: "https://gradepowerlearning.com/wp-content/uploads/2018/09/how-to-use-self-study-scaled.jpeg",
+        price: "Rp. 300.000",
+        level: "advanced",
+        lesson: "10",
+        student: "52",
+        title: "Learning Javascript DOM",
+        rating: "4.9",
+        weeks: "4",
+        language: "Javascript",
+      },
+      {
+        id: 2,
+        img: "https://media.istockphoto.com/id/1581045672/photo/education-laptop-student-computer-teenager-studying-boy-learning-homework-study-young-school.jpg?s=612x612&w=0&k=20&c=ytSpytOADNigrtZTFZkZwYmjBeEculuHc_2MLEvpKNk=",
+        price: "Rp. 250.000",
+        level: "beginner",
+        lesson: "8",
+        student: "45",
+        title: "Introduction to HTML & CSS",
+        rating: "4.7",
+        weeks: "3",
+        language: "HTML & CSS",
+      },
+      {
+        id: 3,
+        img: "https://media.istockphoto.com/id/1214361283/photo/teenage-boy-attending-to-online-school-class.jpg?s=612x612&w=0&k=20&c=1Trs3CU6WXJDJxqWwigcqlhQ6M7motuKqxBH3kKAmLA=",
+        price: "Rp. 400.000",
+        level: "intermediate",
+        lesson: "12",
+        student: "60",
+        title: "Mastering Framework React.js",
+        rating: "4.8",
+        weeks: "5",
+        language: "Javascript",
+      },
+      {
+        id: 4,
+        img: "https://img.freepik.com/premium-photo/group-school-children-using-laptops-class_236854-50313.jpg",
+        price: "Rp. 350.000",
+        level: "advanced",
+        lesson: "15",
+        student: "38",
+        title: "Full-Stack Web Development",
+        rating: "5.0",
+        weeks: "6",
+        language: "Javascript",
+      },
+    ],
+  };
+}
+
+function categories() {
+  return {
+    items: [
+      {
+        name: "Business Management",
+        icon: "business-outline",
+        color: "border-teal-400",
+        text: "text-teal-400"
+      },
+      {
+        name: "Arts and Design Website",
+        icon: "color-palette-outline",
+        color: "border-red-400",
+        text: "text-red-400"
+      },
+      {
+        name: "Computer Science",
+        icon: "desktop-outline",
+        color: "border-yellow-400",
+        text: "text-yellow-400"
+      },
+      {
+        name: "Personal Development",
+        icon: "people-outline",
+        color: "border-green-400",
+        text: "text-green-400"
+      },
+      {
+        name: "Business and Finance",
+        icon: "pie-chart-outline",
+        color: "border-purple-400",
+        text: "text-purple-400"
+      },
+      {
+        name: "Video and Photography",
+        icon: "videocam-outline",
+        color: "border-blue-400",
+        text: "text-blue-400"
+      },
+    ],
+  };
 }
